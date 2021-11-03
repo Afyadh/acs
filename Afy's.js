e6e84ce8e3d1146e82534750a518cef70857e396
Afyadh <73753274+Afyadh@users.noreply.github.com>
2021-11-02 20:27:59 -0700

    Update Afy's.js

--- Afy's.js
+++ Afy's.js
@@ -1,20 +1,17 @@
 let setTransportOptions = this.conn.setTransportOptions;
-              this.conn.setTransportOptions = function (obj) {
-                console.log("Afycord Injected");
-                if (obj.audioEncoder) {
-                  obj.audioEncoder.params = {
-                    surround: "7",
-                  };
-                  obj.audioEncoder.channels = 7;
-                }
-
-                  if (obj.encodingVoiceBitRate < 192000) {
-                      obj.encodingVoiceBitRate = 320000;
-                }
-
-                if (obj.fec) {
-                  obj.fec = false;
-                }
-
-                return setTransportOptions.call(this, obj);
-              };
+  this.conn.setTransportOptions = function (obj) {
+    console.log("Afycord Injected");
+    if (obj.audioEncoder) {
+      obj.audioEncoder.params = {
+        surround: "7",
+      };
+      obj.audioEncoder.channels = 7;
+    }
+      if (obj.encodingVoiceBitRate < 192000) {
+          obj.encodingVoiceBitRate = 320000;
+    }
+    if (obj.fec) {
+      obj.fec = false;
+    }
+    return setTransportOptions.call(this, obj);
+  };
