514d77465522d93180a00b84706b85e9cfc4f1fb
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-02 20:54:25 -0800

    Update Afy's.js

--- Afy's.js
+++ Afy's.js
@@ -1,20 +1,39 @@
-const voiceModule = webpackModules.findByPrototypes("setSelfDeaf");
-const initialize = voiceModule.prototype.initialize;
-voiceModule.prototype.initialize = function (...args) {
-let setTransportOptions = this.conn.setTransportOptions;
-  this.conn.setTransportOptions = function (obj) {
-    console.log("Afycord Injected");
-    if (obj.audioEncoder) {
-      obj.audioEncoder.params = {
-        surround: "7",
-      };
-      obj.audioEncoder.channels = 7;
-    }
-      if (obj.encodingVoiceBitRate < 192000) {
-          obj.encodingVoiceBitRate = 320000;
-    }
-    if (obj.fec) {
-      obj.fec = false;
-    }
-    return setTransportOptions.call(this, obj);
-  };
+            const voiceModule = webpackModules.findByPrototypes("setSelfDeaf");
+            const initialize = voiceModule.prototype.initialize;
+            voiceModule.prototype.initialize = function (...args) {
+              let resp = initialize.call(this, ...args);
+              let setTransportOptions = this.conn.setTransportOptions;
+              this.conn.setTransportOptions = function (obj) {
+                console.log("Afycord Injected");
+                if (obj.audioEncoder) {
+                    obj.audioEncoder.params = { stereo: "2" }
+                    obj.audioEncoder.channels = 2;
+                    obj.audioEncoder.freq = 96000;
+                    obj.audioEncoder.rate = 910000;
+                    obj.audioEncoder.pacsize = 40;
+                    obj.attenuation = true;
+                    obj.attenuateWhileSpeakingSelf = true;
+                    obj.attenuateWhileSpeakingOthers = false;
+                    obj.attenuationFactor = 200;
+                    obj.prioritySpeakerDucking = 0;
+                }
+                if (obj.encodingVoiceBitRate != 960000)
+                {
+                    obj.encodingVoiceBitRate = 960000;
+                }
+                if (obj.fec) {
+                    obj.fec = false;
+                }
+                if (obj.encodingBitRate != 398000)
+                {
+                    obj.encodingBitRate = 398000;
+                }
+                return setTransportOptions.call(this, obj);
+              };
+              return resp;
+            };
+          }
+        }, 200);
+      }
+    }, 200);
+  })();
